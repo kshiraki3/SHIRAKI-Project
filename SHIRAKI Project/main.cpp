6b328e99c64b06f6663424130de08ec1f6e80f4e
白木滉平 <kshiraki2@kshiraki2-no-macbook-air.local>
2016-10-25 16:31:19 +0900

    Initial Commit

---
+++ SHIRAKI Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SHIRAKI Project
+//
+//  Created by 白木滉平 on 2016/10/25.
+//  Copyright © 2016年 SHIRAKI System. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
